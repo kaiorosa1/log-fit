acb9d99382b89185ae69b6541f22e7c47b7f4796
Kaio Rosa <kaiosrosa@gmail.com>
2024-06-21 21:27:32 -0300

    add get all exercises and get exercise by id

--- src/controllers/exercise.ts
+++ src/controllers/exercise.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../index";
 import { Exercise } from "../entities/Exercise";
 import { User } from "../entities/User";
+import { Between, Like } from "typeorm";
 
 
 // Create a new exercise for the authenticated user
@@ -43,3 +44,71 @@ export const createExercise = async (req: Request, res: Response) => {
   }
 };
 
+// Get all exercises for the authenticated user
+export const getAllExercises = async (req: Request, res: Response) => {
+  try {
+   
+    const { title, description, dateStart, dateEnd, duration, tag } = req.query;
+    
+    if (dateStart && dateEnd) {
+      const startDate = new Date(dateStart as string);
+      const endDate = new Date(dateEnd as string);
+      
+      if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
+        return res.status(400).send("Invalid date format. Please use YYYY-MM-DD.");
+      }
+
+      if (startDate > endDate) {
+        return res.status(400).send("Invalid date range. Start date cannot be after end date.");
+      }
+    }
+
+    // Build the query conditions based on the filters
+    const whereConditions: any = { user: req.user };
+    if (title){
+      whereConditions.title = Like(`%${title}%`);
+    } 
+  
+    if (description){
+      whereConditions.description = Like(`%${description}%`);
+    }
+
+    if (dateStart && dateEnd){
+      whereConditions.date = Between(dateStart, dateEnd);
+    }
+    if (duration){
+      whereConditions.duration = duration;
+    } 
+    if (tag){
+      whereConditions.tag = tag;
+    } 
+
+    const exerciseRepository = AppDataSource.getRepository(Exercise);
+    const exercises = await exerciseRepository.find({ where: whereConditions });
+
+    res.status(200).send(exercises);
+  } catch (error) {
+    res.status(500).send("Error getting exercises: " + error);
+  }
+};
+
+// Get a exercise by id for the authenticated user
+export const getExerciseById = async (req: Request, res: Response) => {
+  try {
+   
+    const exerciseId = req.params.id;
+
+    const exerciseRepository = AppDataSource.getRepository(Exercise);
+    const exercise = await exerciseRepository.findOne({
+      where: { id: exerciseId, user: req.user },
+    });
+
+    if (!exercise) {
+      return res.status(404).send("Exercise not found.");
+    }
+
+    res.status(200).send(exercise);
+  } catch (error) {
+    res.status(500).send("Error getting exercise: " + error.message);
+  }
+};
--- src/routes/exercise.ts
+++ src/routes/exercise.ts
@@ -5,6 +5,7 @@ import { authenticate } from "../middleware/auth";
 const router = express.Router();
 
 router.post("/", authenticate, exerciseController.createExercise); 
-
+router.get("/", authenticate, exerciseController.getAllExercises); 
+router.get("/:id", authenticate, exerciseController.getExerciseById);
 
 export default router;
